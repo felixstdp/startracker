de3978d9412ed1898b88b95f0ee7ac912264ebb9
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2018-01-16 12:56:33 +0100

    Update startracker.ino

--- startracker.ino
+++ startracker.ino
@@ -5,10 +5,10 @@
             |RX1   GND| Batteries -
             |GND   RST|
             |GND   VCC| 5V
-       IN1  |2     #A3| 
-       IN2  |3~    #A2| 
-       IN3  |4#    #A1| 
-       IN4  |5~    #A0| 
+            |2     #A3| IN1
+            |3~    #A2| IN2
+            |4#    #A1| IN3
+            |5~    #A0| IN4
             |6~#    15|
             |7      14| 
             |8#     16| 
@@ -17,22 +17,26 @@
 # Analog
 */
 
-byte paso[8] = {B00100000, B00110000, B00010000, B00011000, B00001000, B00001100, B00000100, B00100100};
+// una rotación = 86163581 milisegundos
+
+const int dientes = 60;
+const int gap = 86163581000 / 64 / 8 / dientes;
+byte paso[8] = {B1000, B1100, B0100, B0110, B0010, B0011, B0001, B1001};
 byte i = 0;
-unsigned long timestamp=millis();
+unsigned long now=micros();
 
 void setup()
 {
-  DDRD = DDRD|B00111100;
+  DDRF = DDRF|B00001111;
 }
 
 void loop()
 {
-  if (millis()-timestamp>=1000) // medio paso por segundo
+  if (micros()-now>=gap) // un paso por segundo
   {
-    timestamp=millis();
-    if (timestamp>=4294966796) timestamp-=4294966796;
-    PORTD = paso[i>>5];
+    timestamp=micros();
+    if (now>=4294966796) now-=4294966796;
+    PORTF = paso[i>>5];
     i += 32;
   }  
 } 
